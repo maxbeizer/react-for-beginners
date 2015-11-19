3ce3a8ed93b0b195f7dfe2272b97161a12511837
Max Beizer <max.beizer@gmail.com>
2015-11-19 08:46:09 -0600

    Add part five

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,65 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 /*
+  App
+*/
+
+var App = React.createClass({
+
+  render : function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Order/>
+        <Inventory/>
+      </div>
+    )
+  }
+
+});
+
+/*
+ * Header
+ * <Header/>
+*/
+
+var Header = React.createClass({
+  render : function() {
+    return (
+      <p>Header</p>
+    )
+  }
+});
+
+/*
+ * Order
+ * <Order/>
+*/
+
+var Order = React.createClass({
+  render : function() {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+/*
+ * Inventory
+ * <Inventory/>
+*/
+
+var Inventory = React.createClass({
+  render : function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
+/*
  * Store Picker
  * this will let us make <StorePicker/>
 */
@@ -18,4 +77,4 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
