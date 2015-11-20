96736c8a4b427743596f11ff66c00a5fd5659ebc
Max Beizer <max.beizer@gmail.com>
2015-11-19 22:33:56 -0600

    Add part eight, helper functions

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,8 @@ var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
+var h = require('./helpers');
+
 /*
   App
 */
@@ -83,7 +85,7 @@ var StorePicker = React.createClass({
     return (
       <form className="store-selector">
         <h2>Please enter a store</h2>
-        <input type="text" ref="storeId" required />
+        <input type="text" ref="storeId" defaultValue={h.getFunName()} required />
         <input type="submit" />
       </form>
     )
