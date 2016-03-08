851e5a9616c38f17a38f46892df10e601da17c17
max beizer <max.beizer@gmail.com>
2016-03-07 22:21:35 -0600

    Add part 15, using firebase

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,10 @@ var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers');
 
+// Firebase
+var Rebase = require('re-base');
+var base = Rebase.createClass('https://react-for-beginner.firebaseio.com/');
+
 /*
   App
 */
@@ -19,6 +23,12 @@ var App = React.createClass({
       fishes : {},
       order : {}
     }
+  },
+  componentDidMount : function() {
+    base.syncState(this.props.params.storeId + '/fishes', {
+      context : this,
+      state : 'fishes'
+    });
   },
   addToOrder : function(key) {
     this.state.order[key] = this.state.order[key] + 1 || 1;
